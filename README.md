370a6b82d8bb826112475e00025d78cd2b6de5ba
Atul93 <54507991+Atuls-git@users.noreply.github.com>
2024-01-04 20:46:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# img2emoji
+<p align="center">
+  <img align="center" src="https://img.shields.io/badge/Language_used-Python_&_Bash-magenta" alt="Language" />
+</p>
+
+# Img2emoji
  A python script to convert any type of image into an emoji.
+
+# Created by <img src="https://img.shields.io/badge/Atul-8A2BE2">
+
+# <img src="https://img.shields.io/badge/Usage-Disclaimer-yellow" alt="Disclaimer"/>
+ Converting other people's funny images into 'emoticons' using this script may give rise to ethical and legal implications. It is imperative to uphold the privacy and terms of service of the relevant platforms. If you possess explicit permissions from the image owner, only then should you contemplate deploying this script, ensuring a more ethical and responsible utilization of the tool. I stress that the judicious and ethical application of this tool rests entirely on your own responsibility, and I bear no liability for any misuse.
+
+## Installation instructions
+
+ ### Clone the repo
+  ```bash
+  git clone https://github.com/Atuls-git/img2emoji.git
+  ```
+ ### Move to cloned folder
+  ```bash
+  cd img2emoji
+  ```
+ ### Get permissions
+  ```bash
+  chmod +x *
+  ```
+ ### Install dependencies
+  ```bash
+  ./setup.sh
+  ```
+ ### Run the main file
+  ```bash
+  python3 main.py
+  ```
+Enjoy :)
---
+++ main.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import sys
+import os
+import subprocess
+import time
+
+print("Checking if Colorama has been installed already or not!")
+
+time.sleep(2)
+
+# Check if Colorama has been already installed or not
+try:
+    from colorama import init, Fore
+    print(Fore.LIGHTBLUE_EX + "Colorama has been already installed, We have initialized it for you :)")
+except ImportError:
+    print(Fore.RED + "Colorama has not been installed. Installing it...")
+    subprocess.run(["pip", "install", "colorama"], check=True)
+    from colorama import init, Fore
+
+# Initialize colorama
+init(autoreset=True)
+
+# Check if emoji has been already installed or not
+try:
+    import emoji
+except ImportError:
+    print(Fore.LIGHTRED_EX+ "Emoji not found! Installing it...")
+    subprocess.run(["pip", "install", "emoji"], check=True)
+    import emoji
+
+# Clear the terminal screen
+os.system("clear")
+time.sleep(1)
+
+# Get the terminal size
+rows, columns = os.popen('stty size', 'r').read().split()
+
+# Calculate the center position
+center_row = int(rows) // 2
+center_column = int(columns) // 2
+
+# Set the font size
+font_size = 6
+
+# Print an empty line for top margin
+print()
+
+# Print empty lines for top margin
+for _ in range(center_row - font_size):
+    print()
+
+# Print the welcome message at the center with bigger font size
+print(" " * center_column + Fore.LIGHTMAGENTA_EX + "Welcome\n".center(font_size * 2))
+
+# Print empty lines for bottom margin
+for _ in range(center_row - font_size):
+    print()
+
+time.sleep(2)
+
+# Create a function to prompt the path of the image file which user wants to convert into an emoji
+def input_path():
+    return input("Enter the path of the image file e.g., /path/to/image.png: ")
+
+# Create a function to prompt the size of the emoji user wants
+def output_size():
+    return input("Enter the desired size of the emoji in format of (width, height) e.g., 32, 28: ")
+
+# Create a function to prompt the user to input the path to save the converted image
+def output_path():
+    return input("Enter the path of the output file along with the desired output file name e.g., /path/to/save/file.jpg: ")
+
+# Create function to convert the image into emoji
+def convert_to_emoji(input_path, output_path, output_size):
+    try:
+        # Open the image file
+        img = Image.open(input_path)
+
+        # Validate and parse the output size
+        try:
+            width, height = map(int, output_size.split(','))
+        except ValueError:
+            print("Invalid output size format. Please use the format: width, height")
+            return
+
+        # Create the output directory if it doesn't exist(Error handling)
+        output_dir = os.path.dirname(output_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        # Resize the image to the specified size
+        img_resized = img.resize((width, height))
+
+        # Save the processed image as an emoji file
+        img_resized.save(output_path)
+
+        print(f"Emoji file saved at: {output_path}")
+
+    except FileNotFoundError:
+        print(f"Error: Image file not found at {input_path}")
+    except OSError as e:
+        print(f"Error: {e}")
+
+# Run main function
+if __name__ == "__main__":
+    input_image_path = input_path()
+    output_emoji_path = output_path()
+
+    convert_to_emoji(input_image_path, output_emoji_path)
